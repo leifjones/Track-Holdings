49e62b548ef4dceb71da100db188698cea253bca
Leif Segen <mr.segen@gmail.com>
2013-12-09 11:03:58 -0600

    Initial

---
+++ fifo-lifo-averege-tracking.py
@@ -0,0 +1,10 @@
+# Goals: Track holdings of commodity/currency/stock/etc. to report gains/losses
+# Limitations: Not intended for exchange between multiple currencies
+# Read from CSV
+# Output to TXT
+# Roadmap
+# 0.1:  Read transaction data
+#       Create objects to represent holdings
+#       Track price of each
+# 0.2: Create allow for starting positions
+# 0.3: 
